9eb017d3e7646bd6fafa3f1467a82bf4c24d4aff
Chin Guang <ganchinguang0622@gmail.com>
2026-02-07 13:13:32 +0800

    Fix: Correct hexpire command arguments
    The hexpire command previously had incorrect arguments for the `FIELDS`
    option, causing a Redis error. This commit updates the arguments to
    include the number of fields being expired, resolving the issue.

--- src/modules/redis/index.ts
+++ src/modules/redis/index.ts
@@ -8,7 +8,13 @@ export class RedisModule {
   static async hset(payload: { key: string, field: string, value: string, expireInSec: number }): Promise<void> {
     const { key, field, value, expireInSec } = payload;
     await this.redis.hset(key, field, value);
-    await this.redis.hexpire(key, expireInSec, 'FIELDS', field)
+    // ReplyError: ERR wrong number of arguments for 'hexpire' command
+    //     at parseError (C:\Users\User\Documents\Github\fitness-backend\node_modules\.pnpm\redis-parser@3.0.0\node_modules\redis-parser\lib\parser.js:179:12)
+    //     at parseType (C:\Users\User\Documents\Github\fitness-backend\node_modules\.pnpm\redis-parser@3.0.0\node_modules\redis-parser\lib\parser.js:302:14) {
+    //   command: { name: 'hexpire', args: [ 'user-jwt_token', '900', 'FIELDS', '4' ] }
+    // }
+    const numFields = 1;
+    await this.redis.hexpire(key, expireInSec, 'FIELDS', numFields, field)
   }
 
   static async hget(payload: { key: string, field: string }): Promise<string | null> {
